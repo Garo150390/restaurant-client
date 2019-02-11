4fa3fe503cdd17881c6c3f1d18062d0f60d1d93a
Garo <garo121090@gmail.com>
2019-02-11 22:07:12 +0400

    change validation

--- src/app/pages/restaurants/reservation/reservation.component.ts
+++ src/app/pages/restaurants/reservation/reservation.component.ts
@@ -8,51 +8,10 @@ import { FormControl, FormGroup, Validators } from '@angular/forms';
 })
 export class ReservationComponent implements OnInit {
 
-  public reservForm: FormGroup;
-  public time = {hour: 13, minute: 30};
-
   constructor() {
-    this.reservForm = new FormGroup({
-      'date': new FormControl('', [
-        Validators.required,
-      ]),
-      'time': new FormControl('', [
-        Validators.required,
-      ])
-    });
   }
 
   ngOnInit() {
   }
-
-
-  public validateAllFormFields(formGroup: FormGroup): void {
-    Object.keys(formGroup.controls).forEach(field => {
-      const control = formGroup.get(field);
-
-      if (control instanceof FormControl) {
-        control.markAsTouched({onlySelf: true});
-      } else if (control instanceof FormGroup) {
-        this.validateAllFormFields(control);
-      }
-    });
-  }
-
-  public alertValidate(event) {
-    if (this.reservForm.controls[event.name].status === 'INVALID') {
-      event.classList.add('bg-danger');
-    } else {
-      event.classList.remove('bg-danger');
-    }
-  }
-
-  submitReservForm() {
-    if (this.reservForm.invalid) {
-      this.validateAllFormFields(this.reservForm);
-    } else {
-      console.log(this.reservForm.getRawValue());
-    }
-  }
-
 
 }
